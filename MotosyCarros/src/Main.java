6e2379ca2d747be6c06d6833cc09af5daa222671
betancito <jeronimobeta90@gmail.com>
2024-07-15 20:32:10 -0500

    Updates in workshop

--- MotosyCarros/src/Main.java
+++ MotosyCarros/src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         String[][][] works = new String[maxEmployees][2][4];
         boolean cond = false;
         int numWork = 0;
+        //Ingreso empleados y número de trabajos
         while (cond != true) {
             System.out.println("Ingrese el numero de trabajos a registrar, recuerde que no debe de exeder el numero de mecanicos");
             numWork = scanner.nextInt();
@@ -20,12 +21,32 @@ public class Main {
                 cond = false;
             }
         }
+        //Ingreso información
         for (int i = 0; i < numWork; i++) {
             System.out.println("Ingrese el nombre del empleado");
             String mechName = scanner.next();
             scanner.nextLine();
             System.out.println();
-            System.out.println("");
+            System.out.println("Ingrese el tipo de vehiculo Moto/Carro");
+            String typeVeh= scanner.next();
+            scanner.nextLine();
+            System.out.println();
+            System.out.println("Ingrese la Marca");
+            String brandVeh = scanner.next();
+            scanner.nextLine();
+            System.out.println();
+            System.out.println("Ingrese el modelo");
+            String modVeh = scanner.next();
+            scanner.nextLine();
+            System.out.println();
+            System.out.println("Ingrese el año");
+            String yearVeh = scanner.next();
+            scanner.nextLine();
+            System.out.println();
+            System.out.println("Ingrese el estado Pendiente/En reparación/Reparado)");
+            String status = scanner.next();
+            scanner.nextLine();
+            System.out.println();
         }
 
     }
